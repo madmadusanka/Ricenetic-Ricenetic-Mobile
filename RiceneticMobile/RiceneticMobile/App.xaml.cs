5e9c6bb8a75c25905e3e22b12a3333c3450c7f84
Dilan Madusanka <madmadusanka@gmail.com>
2021-11-19 18:29:32 +0530

    Changed namespace

---
+++ RiceneticMobile/RiceneticMobile/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using RiceneticMobile.ViewModels;
+using RiceneticMobile.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RiceneticMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new HomeView();
+            MainPage.BindingContext = new HomeViewModel();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
